a61102854fb7cf5e1bb1a455d144216b48779c39
john_darwin <40584124+john-darwin@users.noreply.github.com>
2023-04-24 17:09:36 +0800

    Add files via upload

--- aane/main.py
+++ aane/main.py
@@ -1,69 +1,45 @@
-#-----------------------------------------------------------------------------------------------#
-#                                                                                               #
-#   I M P O R T     L I B R A R I E S                                                           #
-#                                                                                               #
-#-----------------------------------------------------------------------------------------------# 
 import numpy as np
-import scipy.sparse as sp
-import networkx as nx
-import pickle
+import scipy.io as sio
+from scipy import sparse
+from AANE import AANE
 import time
-import argparse
+import pandas as pd
 
-from model import AANE
+'''################# Load data  #################'''
+lambd = 1  # the regularization parameter
+rho = 5  # the penalty parameter
 
-#-----------------------------------------------------------------------------------------------#
-#                                                                                               #
-#   AANE                                                                                        #
-#                                                                                               #
-#-----------------------------------------------------------------------------------------------#
-def aane(args):
-	start_time = time.time()
+# mat_contents = sio.loadmat('Flickr.mat')
+# lambd = 0.0425  # the regularization parameter
+# rho = 4  # the penalty parameter
 
-	# Init graph
-	g_npz = sp.load_npz(args.input)
-	G = sp.csc_matrix(g_npz) # column slicing is operated on csc matrices
-	
-	n = G.shape[0]
-	print("\nNumber of nodes: {}".format(n))
+'''################# Experimental Settings #################'''
+d = 150  # the dimension of the embedding representation
+G=pd.read_csv('D:\Code\pycharm\AANE对比实验/network.csv',header=None)
+G = sparse.csc_matrix(G)   # Here's the initialization of the sparse matrix.
+A=pd.read_excel('D:\Code\pycharm\AANE对比实验\preprocess/attri.xlsx',header=None)
+A = sparse.csc_matrix(A)
+n = G.shape[0]
+Indices = np.random.randint(25, size=n)+1  # 5-fold cross-validation indices
 
-	indexList = np.random.randint(25, size=n)  # 5-fold cross-validation indices
+Group1 = []
+Group2 = []
+[Group1.append(x) for x in range(0, n) if Indices[x] <= 20]  # 2 for 10%, 5 for 25%, 20 for 100% of training group
+[Group2.append(x) for x in range(0, n) if Indices[x] >= 21]  # test group
+n1 = len(Group1)  # num of nodes in training group
+n2 = len(Group2)  # num of nodes in test group
+CombG = G[Group1+Group2, :][:, Group1+Group2]
+CombA = A[Group1+Group2, :]
 
-	Group1 = []
-	Group2 = []
-	[Group1.append(x) for x in range(0, n) if indexList[x] <= 20]  # 2 for 10%, 5 for 25%, 20 for 100% of training group
-	[Group2.append(x) for x in range(0, n) if indexList[x] >= 21]  # test group
-	n1 = len(Group1)  # num of nodes in training group
-	n2 = len(Group2)  # num of nodes in test group
-	G_matrix = G[Group1+Group2, :][:, Group1+Group2]
+'''################# Accelerated Attributed Network Embedding #################'''
+print("Accelerated Attributed Network Embedding (AANE), 5-fold with 100% of training is used:")
+start_time = time.time()
+H_AANE = AANE(CombG, CombA, d, lambd, rho).function()
+print("time elapsed: {:.2f}s".format(time.time() - start_time))
 
-	# Launch AANE
-	print("\nInitialization ...")
-	H_Net = AANE(G_matrix, G_matrix, args).run()
-
-	# Save to output file
-	print("----- Total time {:.2f}s -----".format(time.time() - start_time))
-	pickle.dump(H_Net, open(args.output, 'wb'))
-	return
-
-#-----------------------------------------------------------------------------------------------#
-#                                                                                               #
-#   MAIN                                                                                        #
-#   Parse input args                                                                            #
-#                                                                                               #
-#-----------------------------------------------------------------------------------------------# 
-def main():
-  parser = argparse.ArgumentParser()
-  parser.add_argument('--input', nargs='?', required=True)
-  parser.add_argument('--output', required=True)
-  parser.add_argument('--iter', default=5)
-  parser.add_argument('--dimension', default=128)
-  parser.add_argument('--lambd', default=0.05)
-  parser.add_argument('--rho', default=5)
-
-  args = parser.parse_args()
-
-  aane(args)
-
-if __name__ == "__main__":
-  main()
+'''################# AANE for a Pure Network #################'''
+print("AANE for a pure network:")
+start_time = time.time()
+H_Net = AANE(CombG, CombG, d, lambd, rho).function()
+print("time elapsed: {:.2f}s".format(time.time() - start_time))
+sio.savemat('Embedding.mat', {"H_AANE": H_AANE, "H_Net": H_Net})
